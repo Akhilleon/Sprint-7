70f2b97cc104860e50f3d84ab51c0fa0f8dbaf07
Néstor <nestor_moreno098@hotmail.com>
2023-02-04 01:28:10 +0100

    Nivel 2 completado

--- sprint-7/src/components/Counter.vue
+++ sprint-7/src/components/Counter.vue
@@ -1,16 +1,99 @@
 <template>
     <div id="counter">
-        <div class="choices">
-            <label>Number of pages </label>
-            <button @click.prevent="counterPages++">+</button>
-            <input class="inputBoxes" type="number" min="0" v-model="counterPages">
-            <button @click.prevent="counterPages > 1 ? counterPages-- : counterPages = 1">-</button>
+      <!-- Modal One -->
+        <transition enter-active-class="ease-out duration-300" 
+          enter-class="opacity-0" enter-to-class="opacity-100"
+          leave-active-class="ease-in duration-200" leave-class="opacity-100" leave-to-class="opacity-0">
+          <div v-show="modalOne" class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">          
+            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>          
+          <div class="fixed inset-0 z-10 overflow-y-auto">
+            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <transition enter-active-class="ease-out duration-300" 
+              enter-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to-class="opacity-100 translate-y-0 sm:scale-100"
+              leave-active-class="ease-in duration-200" leave-class="opacity-100 translate-y-0 sm:scale-100" leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+              <div v-show="modalOne" class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div class="sm:flex sm:items-start">
+                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <div class="mt-2">
+                        <p class="text-sm text-gray-900">En este componente debe indicar el número de páginas que tendrá su sitio web</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button @click="modalOne = false" type="button" class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cerrar</button>
+                </div>
+              </div>
+            </transition>
+            </div>
+          </div>
         </div>
-        <div class="choices">
+        </transition>
+        <!-- End of Modal One -->
+        <!-- Modal Two -->
+        <transition enter-active-class="ease-out duration-300" 
+          enter-class="opacity-0" enter-to-class="opacity-100"
+          leave-active-class="ease-in duration-200" leave-class="opacity-100" leave-to-class="opacity-0">
+          <div v-show="modalTwo" class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true" >
+            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+          <div class="fixed inset-0 z-10 overflow-y-auto">
+            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">              
+              <transition enter-active-class="ease-out duration-300" 
+              enter-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to-class="opacity-100 translate-y-0 sm:scale-100"
+              leave-active-class="ease-in duration-200" leave-class="opacity-100 translate-y-0 sm:scale-100" leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+              <div v-show="modalTwo" class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div class="sm:flex sm:items-start">
+                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <div class="mt-2">
+                        <p class="text-sm text-gray-900">En este componente debe indicar el número de idiomas que tendrá su sitio web</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button type="button" @click="modalTwo = false" class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cerrar</button>
+                </div>
+              </div>
+            </transition>
+            </div>
+          </div>
+        </div>
+        </transition>
+        <!-- End of Modal Two -->
+
+        <div class="choices space-x-1">
+            <label>Number of pages </label>
+            <button @click.prevent="counterPages++">
+              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="inline-flex items-center" viewBox="0 0 20 20">
+              <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
+              </svg>
+            </button>
+            <input class="inputBoxes" type="number" min="0" v-model="counterPages">
+            <button @click.prevent="counterPages > 1 ? counterPages-- : counterPages = 1">
+              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="inline-flex items-center" viewBox="0 0 20 20">
+              <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm2.5 7.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1z"/>
+              </svg>
+            </button><button type="button" @click.prevent="modalOne = true" 
+            class="rounded inline-flex items-center"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26" width="16px" height="16px"><path d="M 13 1.1875 C 6.476563 1.1875 1.1875 6.476563 1.1875 13 C 1.1875 19.523438 6.476563 24.8125 13 24.8125 C 19.523438 24.8125 24.8125 19.523438 24.8125 13 C 24.8125 6.476563 19.523438 1.1875 13 1.1875 Z M 15.460938 19.496094 C 14.851563 19.734375 14.367188 19.917969 14.003906 20.042969 C 13.640625 20.167969 13.222656 20.230469 12.742188 20.230469 C 12.007813 20.230469 11.433594 20.050781 11.023438 19.691406 C 10.617188 19.335938 10.414063 18.878906 10.414063 18.324219 C 10.414063 18.109375 10.429688 17.890625 10.460938 17.667969 C 10.488281 17.441406 10.539063 17.191406 10.605469 16.90625 L 11.367188 14.21875 C 11.433594 13.960938 11.492188 13.71875 11.539063 13.488281 C 11.585938 13.257813 11.605469 13.046875 11.605469 12.855469 C 11.605469 12.515625 11.535156 12.273438 11.394531 12.140625 C 11.25 12.003906 10.980469 11.9375 10.582031 11.9375 C 10.386719 11.9375 10.183594 11.96875 9.976563 12.027344 C 9.769531 12.089844 9.59375 12.148438 9.445313 12.203125 L 9.648438 11.375 C 10.144531 11.171875 10.621094 11 11.078125 10.855469 C 11.53125 10.710938 11.964844 10.636719 12.367188 10.636719 C 13.097656 10.636719 13.664063 10.816406 14.058594 11.167969 C 14.453125 11.519531 14.652344 11.980469 14.652344 12.542969 C 14.652344 12.660156 14.640625 12.867188 14.613281 13.160156 C 14.585938 13.453125 14.535156 13.722656 14.460938 13.972656 L 13.703125 16.652344 C 13.640625 16.867188 13.585938 17.113281 13.535156 17.386719 C 13.488281 17.660156 13.464844 17.871094 13.464844 18.011719 C 13.464844 18.367188 13.542969 18.613281 13.703125 18.742188 C 13.859375 18.871094 14.136719 18.933594 14.53125 18.933594 C 14.714844 18.933594 14.921875 18.902344 15.15625 18.839844 C 15.386719 18.773438 15.554688 18.71875 15.660156 18.667969 Z M 15.324219 8.617188 C 14.972656 8.945313 14.546875 9.109375 14.050781 9.109375 C 13.554688 9.109375 13.125 8.945313 12.769531 8.617188 C 12.414063 8.289063 12.238281 7.890625 12.238281 7.425781 C 12.238281 6.960938 12.417969 6.558594 12.769531 6.226563 C 13.125 5.894531 13.554688 5.730469 14.050781 5.730469 C 14.546875 5.730469 14.972656 5.894531 15.324219 6.226563 C 15.679688 6.558594 15.855469 6.960938 15.855469 7.425781 C 15.855469 7.890625 15.679688 8.289063 15.324219 8.617188 Z"/></svg></button>
+        </div>
+        <div class="choices space-x-1">
             <label>Number of languages </label>
-            <button @click.prevent="counterLanguages++">+</button>
+            <button @click.prevent="counterLanguages++">
+              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="inline-flex items-center" viewBox="0 0 20 20">
+              <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
+              </svg>
+            </button>
             <input class="inputBoxes" type="number" min="0" v-model="counterLanguages">
-            <button @click.prevent="counterLanguages > 1 ? counterLanguages-- : counterLanguages = 1">-</button>
+            <button @click.prevent="counterLanguages > 1 ? counterLanguages-- : counterLanguages = 1">
+              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="inline-flex items-center" viewBox="0 0 20 20">
+              <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm2.5 7.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1z"/>
+              </svg>
+            </button>
+            <button type="button" @click.prevent="modalTwo = true"
+            class="rounded inline-flex items-center"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26" width="16px" height="16px">
+              <path d="M 13 1.1875 C 6.476563 1.1875 1.1875 6.476563 1.1875 13 C 1.1875 19.523438 6.476563 24.8125 13 24.8125 C 19.523438 24.8125 24.8125 19.523438 24.8125 13 C 24.8125 6.476563 19.523438 1.1875 13 1.1875 Z M 15.460938 19.496094 C 14.851563 19.734375 14.367188 19.917969 14.003906 20.042969 C 13.640625 20.167969 13.222656 20.230469 12.742188 20.230469 C 12.007813 20.230469 11.433594 20.050781 11.023438 19.691406 C 10.617188 19.335938 10.414063 18.878906 10.414063 18.324219 C 10.414063 18.109375 10.429688 17.890625 10.460938 17.667969 C 10.488281 17.441406 10.539063 17.191406 10.605469 16.90625 L 11.367188 14.21875 C 11.433594 13.960938 11.492188 13.71875 11.539063 13.488281 C 11.585938 13.257813 11.605469 13.046875 11.605469 12.855469 C 11.605469 12.515625 11.535156 12.273438 11.394531 12.140625 C 11.25 12.003906 10.980469 11.9375 10.582031 11.9375 C 10.386719 11.9375 10.183594 11.96875 9.976563 12.027344 C 9.769531 12.089844 9.59375 12.148438 9.445313 12.203125 L 9.648438 11.375 C 10.144531 11.171875 10.621094 11 11.078125 10.855469 C 11.53125 10.710938 11.964844 10.636719 12.367188 10.636719 C 13.097656 10.636719 13.664063 10.816406 14.058594 11.167969 C 14.453125 11.519531 14.652344 11.980469 14.652344 12.542969 C 14.652344 12.660156 14.640625 12.867188 14.613281 13.160156 C 14.585938 13.453125 14.535156 13.722656 14.460938 13.972656 L 13.703125 16.652344 C 13.640625 16.867188 13.585938 17.113281 13.535156 17.386719 C 13.488281 17.660156 13.464844 17.871094 13.464844 18.011719 C 13.464844 18.367188 13.542969 18.613281 13.703125 18.742188 C 13.859375 18.871094 14.136719 18.933594 14.53125 18.933594 C 14.714844 18.933594 14.921875 18.902344 15.15625 18.839844 C 15.386719 18.773438 15.554688 18.71875 15.660156 18.667969 Z M 15.324219 8.617188 C 14.972656 8.945313 14.546875 9.109375 14.050781 9.109375 C 13.554688 9.109375 13.125 8.945313 12.769531 8.617188 C 12.414063 8.289063 12.238281 7.890625 12.238281 7.425781 C 12.238281 6.960938 12.417969 6.558594 12.769531 6.226563 C 13.125 5.894531 13.554688 5.730469 14.050781 5.730469 C 14.546875 5.730469 14.972656 5.894531 15.324219 6.226563 C 15.679688 6.558594 15.855469 6.960938 15.855469 7.425781 C 15.855469 7.890625 15.679688 8.289063 15.324219 8.617188 Z"/></svg></button>
         </div>
     </div>
 </template>
@@ -21,7 +104,9 @@
         data() {
             return {
                 counterPages: 1,
-                counterLanguages: 1
+                counterLanguages: 1,
+                modalOne: false,
+                modalTwo: false
             }
         },
         props: {
--- sprint-7/src/components/Home.vue
+++ sprint-7/src/components/Home.vue
@@ -12,17 +12,31 @@
         <input type="checkbox" value="200" v-model="price"><label>Una campaña de Google Ads (200€)</label>
       </div>
 
-      <p>Precio: {{ total }}€</p>
+      <p>Precio: {{ total }} €</p>
+
+      <div>
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre Presupuesto:</label>
+        <input type="text" v-model="name" class="block mx-auto p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      </div>
+      <div>
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cliente</label>
+        <input type="text" v-model="client" class="block mx-auto p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      </div>
+
+      <button @click.prevent="mountList()" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Enviar</button>
+
+      <ListItem :pressupostList="this.pressupostList" class="absolute inset-y-10 right-20 w-42 mt-0"></ListItem>
     </form>
 </template>
 
 <script>
 import PanelItem from './Panel.vue'
+import ListItem from './List.vue'
 
 export default {
   name: 'HomeForm',
   components: {
-    PanelItem
+    PanelItem, ListItem
   },
   data() {
     return {
@@ -30,7 +44,10 @@ export default {
       total: 0,
       pages: 1,
       languages: 1,
-      enabled: false
+      enabled: false,
+      name: '',
+      client: '',
+      pressupostList: []
     }
   },
   watch: {
@@ -45,7 +62,7 @@ export default {
     },
     enabled: function() {
       this.calculateTotal();
-    }
+    },
   },
   methods: {
     calculateTotal() {
@@ -54,6 +71,36 @@ export default {
       let totalSum = sum + panelSum
       let result = this.total = totalSum;
       return result;      
+    },
+    mountList() {
+      const pressupost = {
+        name: this.name,
+        client: this.client,
+        services: [],
+        pages: this.pages,
+        languages: this.languages,
+        total: this.total 
+      }
+      if (this.name && this.client != '' && this.total != 0) {
+        this.pressupostList.push(pressupost);
+      }
+
+      if (this.enabled == false) {
+        pressupost.pages = 0;
+        pressupost.languages = 0;
+      }
+
+      if (this.price.includes('500')) {
+        pressupost.services.push("Página web");
+      } 
+      
+      if (this.price.includes('300')) {
+        pressupost.services.push("Consultoria SEO");
+      } 
+      
+      if (this.price.includes('200')) {
+        pressupost.services.push("Campaña Google ADs");
+      }
     }
   }
 }
